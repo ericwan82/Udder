617fc898bf7fe4430a39cc4c8dc3b6dbbaeaa3a0
EricWan <ericwan82@gmail.com>
2021-04-17 09:08:55 -0600

    Updated label

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Eric Wan on 4/17/21.
+//  Copyright © 2021 Eric Wan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
